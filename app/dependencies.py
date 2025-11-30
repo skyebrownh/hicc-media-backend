e81fbf8d48c2c1eaf77bc8d0dcfbff767d7c5ac1
Skye Brown <skye.brownh@gmail.com>
2025-11-30 17:57:51 -0500

    simplify FastAPI app for testing, fix api key dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,18 @@
-from app.utils.supabase import SupabaseService 
-from app.utils.env import SUPABASE_URL, SUPABASE_API_KEY
+from fastapi import Request, HTTPException, status
 
-def get_supabase_service():
-    return SupabaseService(url=SUPABASE_URL, key=SUPABASE_API_KEY)
+from app.utils.env import FAST_API_KEY
+
+# from app.utils.supabase import SupabaseService 
+# from app.utils.env import SUPABASE_URL, SUPABASE_API_KEY
+
+# def get_supabase_service():
+#     return SupabaseService(url=SUPABASE_URL, key=SUPABASE_API_KEY)
+
+def verify_api_key(request: Request):
+    api_key = request.headers.get("x-api-key")
+
+    if not api_key or api_key.strip().lower() != FAST_API_KEY:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Unauthorized: Invalid or missing API Key"
+        )
--- app/main.py
+++ app/main.py
@@ -1,31 +1,23 @@
-from fastapi import FastAPI, Request, HTTPException, status
+from fastapi import FastAPI, Depends
 
-from app.routers import * 
-from app.utils.env import FAST_API_KEY
+from app.dependencies import verify_api_key
+# from app.routers import * 
 
 app = FastAPI()
-
-@app.middleware("http")
-async def verify_api_key(request: Request, call_next):
-    api_key = request.headers.get("x-api-key")
-    if not api_key:
-        api_key = ""
-
-    if api_key.strip().lower() != FAST_API_KEY:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized: Invalid or missing API Key")
     
-    response = await call_next(request)
-    return response
+@app.get("/", dependencies=[Depends(verify_api_key)])
+async def root():
+    return {"message": "Hello World!"}
 
-app.include_router(user_router)
-app.include_router(team_router)
-app.include_router(media_role_router)
-app.include_router(proficiency_level_router)
-app.include_router(schedule_date_type_router)
-app.include_router(date_router)
-app.include_router(schedule_router)
-app.include_router(user_availability_router)
-app.include_router(team_user_router)
-app.include_router(user_role_router)
-app.include_router(schedule_date_router)
-app.include_router(schedule_date_role_router)
+# app.include_router(user_router)
+# app.include_router(team_router)
+# app.include_router(media_role_router)
+# app.include_router(proficiency_level_router)
+# app.include_router(schedule_date_type_router)
+# app.include_router(date_router)
+# app.include_router(schedule_router)
+# app.include_router(user_availability_router)
+# app.include_router(team_user_router)
+# app.include_router(user_role_router)
+# app.include_router(schedule_date_router)
+# app.include_router(schedule_date_role_router)
