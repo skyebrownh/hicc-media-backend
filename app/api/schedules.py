c4a7dd6ba6c87ab90730627dc1e39fde4510c7f8
Skye Brown <skye.brownh@gmail.com>
2026-01-03 21:53:52 -0500

    refactor models to be split by table

--- app/api/schedules.py
+++ app/api/schedules.py
@@ -1,27 +1,29 @@
 from uuid import UUID
-from fastapi import APIRouter, Depends, Body, status
-from app.db.models import Schedule, ScheduleCreate, ScheduleUpdate
+from fastapi import APIRouter, Depends, Body, status, HTTPException
+from app.db.models import Schedule, ScheduleCreate, ScheduleUpdate, ScheduleGridPublic, EventWithAssignmentsPublic, Event, EventAssignment
 from sqlmodel import Session, select
 from app.utils.dependencies import get_db_session
 from app.utils.helpers import get_or_404
+from app.services.scheduling import get_schedule
 
 router = APIRouter(prefix="/schedules")
 
 @router.get("", response_model=list[Schedule])
-async def get_schedules(session: Session = Depends(get_db_session)):
+async def get_all_schedules(session: Session = Depends(get_db_session)):
     """Get all schedules"""
     return session.exec(select(Schedule)).all()
 
 @router.get("/{id}", response_model=Schedule)
-async def get_schedule(id: UUID, session: Session = Depends(get_db_session)):
+async def get_single_schedule(id: UUID, session: Session = Depends(get_db_session)):
     """Get a schedule by ID"""
     return get_or_404(session, Schedule, id)
 
-# @router.get("/{schedule_id}/schedule_dates", response_model=list[ScheduleDateOut])
-# async def get_schedule_dates_for_schedule(schedule_id: UUID, conn: asyncpg.Connection = Depends(get_db_connection)):
-#     # Verify schedule exists first (will raise 404 if not found)
-#     await fetch_one(conn, table="schedules", filters={"schedule_id": schedule_id})
-#     return await fetch_all(conn, table="schedule_dates", filters={"schedule_id": schedule_id})
+# @router.get("/{id}/grid", response_model=list[ScheduleGridPublic])
+# async def get_schedule_grid(id: UUID, session: Session = Depends(get_db_session)):
+#     """Get a schedule grid by ID"""
+#     schedule = get_schedule(session, id)
+#     if not schedule:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Schedule not found")
 
 # @router.post("", response_model=ScheduleOut, status_code=status.HTTP_201_CREATED)
 # async def post_schedule(schedule: ScheduleCreate, conn: asyncpg.Connection = Depends(get_db_connection)):
